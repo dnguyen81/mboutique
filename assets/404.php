ba76d31d87fbadd0dba1874d48dd65613ebd4b60
dnguyen81git <davidhuynguyen81@gmail.com>
2015-11-30 22:32:04 -0800

    changed wording for 404.php

--- assets/404.php
+++ assets/404.php
@@ -1,4 +1,4 @@
 <div id="welcome">
-    <h3>Sorry, the page you requested is not here</h3>
+    <h3>Sorry, the page you are looking for is not here</h3>
     <img src="assets/images/404.jpg">
 </div>
